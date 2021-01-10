a164bd3ad99c661aa6e7e5644d3cb179e720b866
funhappyit <fun.happyit@gmail.com>
2021-01-10 17:22:48 +0900

    끝말 잇기 커밋

--- react-webgame/gugudan/webpack.config.js
+++ react-webgame/gugudan/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 module.exports = {
   mode: "development",
   devtool: "eval", //개발용일때=> hidden-source-map
@@ -14,7 +15,18 @@ module.exports = {
         test: /\.jsx?/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 1% in KR", "last 2 chrome versions"], //browerslist
+                },
+                debug: true,
+              },
+            ],
+            "@babel/preset-react",
+          ],
           plugins: ["@babel/plugin-proposal-class-properties"],
         },
       },
--- react-webgame/word-relay/webpack.config.js
+++ react-webgame/word-relay/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const RefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 module.exports = {
   name: "wordrelay-setting",
   mode: "development", //실서비스 : production
@@ -16,14 +17,35 @@ module.exports = {
         test: /\.jsx?/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-          plugins: ["@babel/plugin-proposal-class-properties"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: {
+                  browsers: ["> 1% in KR", "last 2 chrome versions"], //browerslist
+                },
+                debug: true,
+              },
+            ],
+            "@babel/preset-react",
+          ],
+          plugins: [
+            "@babel/plugin-proposal-class-properties",
+            "react-refresh/babel",
+          ],
         },
       },
     ],
   },
+  plugins: [new RefreshWebpackPlugin()],
   output: {
     path: path.join(__dirname, "dist"), //경로
     filename: "app.js",
+    publicPath: "/dist/",
   }, //출력
+  //dev서버 설정-> dist폴더에 결과물을 저장해준다. 변경점 감지해 줄 수 있음
+  devServer: {
+    publicPath: "/dist/",
+    hot: true,
+  },
 };
