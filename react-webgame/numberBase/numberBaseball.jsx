ba290b05c58c4d6a0110c6a638045c38bb6b1fad
funhappyit <fun.happyit@gmail.com>
2021-01-13 08:36:29 +0900

    숫자야구 커밋

--- react-webgame/numberBase/numberBaseball.jsx
+++ react-webgame/numberBase/numberBaseball.jsx
@@ -3,7 +3,13 @@ import React,{ Component } from 'react';
 import Try from './Try';
 
 function getNumbers(){//숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수 
-
+    const candidate = [1,2,3,4,5,6,7,8,9];
+    const array = [];
+    for(let i=0; i<4;i+=1){
+        const chosen = candidate.splice(Math.floor(Math.random()*(9 - i)),1)[0];
+        array.push(chosen);
+    }
+    return array;
 }
 class numberBaseball extends Component {
     state = {
@@ -12,11 +18,24 @@ class numberBaseball extends Component {
         answer: getNumbers(),
         tries:[],
     };
-    onSubmitForm = () => {
-
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        if(this.state.value === this.state.answer.join('')){
+            this.setState({
+                result:'홈런',
+                tries: [...this.state.tries, {try:this.state.value, result:'홈런!'}],
+            })
+        }else{
+            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+    
+        }
     };
-    onChangeInput = () =>{
-
+    onChangeInput = (e) =>{
+        this.setState({
+            value: e.target.value,
+        });
     };
     fruits = [
         {fruit:'사과',taste:'맛있다'},
@@ -30,6 +49,7 @@ class numberBaseball extends Component {
            <>
                 <h1>{this.state.result}</h1>
                 <form onSubmit={this.onSubmitForm}>
+                    {/* 주석 */}
                     <input maxLength={4} value={this.state.value} onChange={this.onChangeInput}/>   
                 </form>
                 <div>시도:{this.state.tries.length}</div>
