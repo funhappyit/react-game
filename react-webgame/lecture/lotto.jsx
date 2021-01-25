86f80f84e34338813f297863c268cd558137a0d2
funhappyit <fun.happyit@gmail.com>
2021-01-25 22:16:00 +0900

    리액트 로또 커밋

--- react-webgame/lecture/lotto.jsx
+++ react-webgame/lecture/lotto.jsx
@@ -3,9 +3,10 @@ import Ball from './Ball';
 
 //반복 실행되는 문제가 발생할 수 있음 
 function getWinNumbers(){
-    console.log('getWinNumbers');
+    
     const candidate = Array(45).fill().map((v,i)=> i+1);
     const shuffle = [];
+    
     while(candidate.length > 0){
         //셔플 방법 
         shuffle.push(candidate.splice(Math.floor(Math.random()*candidate.length),1)[0]);
@@ -22,32 +23,56 @@ class Lotto extends Component {
         redo: false,
     };
     timeouts = [];
+
+    runTimeouts = () => {
+         //this.state로 구조분해 
+         const {winNumbers} = this.state;
+         //비동기와 변수와 같이 쓰면 클로져 문제가 생기는데 let을 쓰면 안생김
+         for(let i=0; i< winNumbers.length -1; i++){
+         this.timeouts[i] = setTimeout(() => {
+             this.setState((prevState) => {
+                 return{
+                     winBalls: [...prevState.winBalls, winNumbers[i]],
+                 };
+             });
+            }, (i + 1)*1000);
+         }
+         this.timeouts[6] = setTimeout(()=>{
+             this.setState({
+                 bonus:winNumbers[6],
+                 redo:true,
+             });
+         }, 7000);
+    }
+
     //컴포넌트 시작하자마자 실행
     componentDidMount(){
-        //this.state로 구조분해 
-        const {winNumbers} = this.state;
-        //비동기와 변수와 같이 쓰면 클로져 문제가 생기는데 let을 쓰면 안생김
-        for(let i=0; i< winNumbers.length -1; i++){
-        this.timeouts[i] = setTimeout(() => {
-            this.setState((prevState) => {
-                return{
-                    winBalls: [...prevState.winBalls, winNumbers[i]],
-                };
-            });
-           }, (i + 1)*1000);
+       this.runTimeouts();
+    }
+    //조건문으로 감싸줘야지만 필요할때만 업데이트
+    componentDidUpdate(prevProps, prevState){
+        console.log('didUpdate');
+        if(this.state.winBalls.length === 0){
+           
+            this.runTimeouts();
         }
-        this.timeouts[6] = setTimeout(()=>{
-            this.setState({
-                bonus:winNumbers[6],
-                redo:true,
-            });
-        }, 7000)
     }
     //정리 메모리 누수 문제 해결 
     componentWillUnmount(){
         this.timeouts.forEach((v)=>{
             clearTimeout(v);
         });
+    }
+    //onClickRedo
+    onClickRedo = () => {
+        console.log('onClickRedo');
+        this.setState({
+            winNumbers: getWinNumbers(), //당첨 숫자들
+            winBalls: [],
+            bonus: null,//보너스 공 
+            redo: false,
+        });
+        this.timeouts = [];
     }
     render(){
         const {winBalls, bonus, redo} = this.state;
@@ -63,7 +88,7 @@ class Lotto extends Component {
             </div>
             <div>보너스!</div>
             {bonus && <Ball number={bonus}/>}
-            <button onClick={redo ? this.onClickRedo: () => {}}>한 번 더</button>
+            {redo && <button onClick={this.onClickRedo}>한 번 더</button>}
           </>
         );
     }
